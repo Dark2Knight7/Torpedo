e34e18af8ffdc9ea7903c603f1e53fd1cea6c619
Darki2Knight6 <ducpeti27@gmail.com>
2022-05-16 15:29:34 +0200

    metódusok létrejötte

--- Projekt/Torpedo/src/torpedo/Torpedo.java
+++ Projekt/Torpedo/src/torpedo/Torpedo.java
@@ -1,5 +1,7 @@
 package torpedo;
 
+import java.util.Scanner;
+
 public class Torpedo {
     
     public static int palyaMezok=7;
@@ -10,6 +12,10 @@ public class Torpedo {
     }
     
     public static void tamadas(){
-    
+        Scanner tipp = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Kérem adja meg melyik mezőre szeretne támadni: ");
+
+        String tamadasiMezo = tipp.nextLine();  // Read user input
+        System.out.println("A mező ahova támadott: " + tamadasiMezo);  // Output user input
     }
 }
