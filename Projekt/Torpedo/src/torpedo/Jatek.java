fdf5800b03b37853568969b3d1b07ca56efddd00
gavrisattila <gavrisa01@gmail.com>
2022-05-16 15:54:53 +0200

    pálya létrehozás 2

--- Projekt/Torpedo/src/torpedo/Jatek.java
+++ Projekt/Torpedo/src/torpedo/Jatek.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package torpedo;
 
 import static torpedo.Torpedo.palyaGeneralas;
 import static torpedo.Torpedo.tamadas;
 
-/**
- *
- * @author hallgato
- */
 public class Jatek {
     public static void main(String[] args) {
         palyaGeneralas();
