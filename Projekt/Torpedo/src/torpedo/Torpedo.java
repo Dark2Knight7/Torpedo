7355dcbdb095ecc20f30873e6636483d47581912
Darki2Knight6 <ducpeti27@gmail.com>
2022-05-16 15:21:53 +0200

    metódusok létrejötte

--- Projekt/Torpedo/src/torpedo/Torpedo.java
+++ Projekt/Torpedo/src/torpedo/Torpedo.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package torpedo;
 
-/**
- *
- * @author hallgato
- */
 public class Torpedo {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public static int palyaMezok=7;
+    public static int[] palya = new int[palyaMezok];
+    
+    public static void palyaGeneralas(){
+    
     }
     
+    public static void tamadas(){
+    
+    }
 }
